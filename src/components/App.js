3b1c963ab6ec9730f8b62a10cc3ffb164a522493
GimYoungPhil <youngphil.gim@gmail.com>
2019-09-04 08:19:58 +0900

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import AddTodo from '../containers/AddTodo';
 const App = () => (
   <div className="container">
     <div className="row justify-content-center">
-      <div className="col-6">
+      <div className="col-12 col-md-6">
         <h4>Todo App</h4>
         <AddTodo />
         <VisibleTodoList />
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Todo = ({ onClick, completed, text }) => (
-  <div className={completed ? 'alert alert-success' : 'alert alert-primary'} role="alert" onClick={onClick}>
+  <div
+    className={completed ? 'alert alert-success' : 'alert alert-primary'}
+    role="alert"
+    onClick={onClick}
+  >
     {text}
   </div>
 );
